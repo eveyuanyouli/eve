8512fe26982c8b8b3ee0982d9dd4accd0babdd42
eve <17711578329@163.com>
2019-01-26 22:32:05 +0800

    測試通過

--- src/main/java/org/seckill/web/TestFreeController.java
+++ src/main/java/org/seckill/web/TestFreeController.java
@@ -1,13 +1,18 @@
 package org.seckill.web;
 
+import org.seckill.entity.TestFree;
 import org.seckill.service.TestFreeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/")
@@ -18,8 +23,25 @@ public class TestFreeController {
 
     @RequestMapping(value = "/query")
     public String query(Model model, HttpServletRequest request) {
-        model.addAttribute("name", "The Flash");
+        List<TestFree> testFrees = testFreeService.queryAll();
+
+        model.addAttribute("name", testFrees.get(0).getName());
+        model.addAttribute("list", testFrees);
         return "query";
+
+    }
+
+    @RequestMapping(value = "/get")
+    @ResponseBody
+    public String getQueryValue(HttpServletRequest request) {
+        Map<String, Object> map = new HashMap<>();
+        String name = request.getParameter("name");
+        if (null != name) {
+            map.put("success", name);
+            return "1";
+        } else {
+            return "0";
+        }
     }
 
 }
--- src/main/webapp/WEB-INF/jsp/query.jsp
+++ src/main/webapp/WEB-INF/jsp/query.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: asus
@@ -9,9 +10,46 @@
 <html>
 <head>
     <title>query</title>
+    <%@include file="common/head.jsp" %>
 </head>
 
 <body>
 <h3 align="center"> ${name} </h3>
+
+<c:forEach var="eve" items="${list}">
+    <tr>
+        <td>${eve.name}</td>
+        <td>${eve.des}</td>
+    </tr>
+    <a onclick="forword('${name}')">Click</a>
+</c:forEach>
+
+<script type="text/javascript">
+    function forword(e) {
+        $.ajax({
+            url: "get.action",
+            type: "GET",
+            data: {
+                name: e
+            },
+            dataType: "json",
+            success: function (res) {
+                if (res === 1) {
+                    alert("Success !" + res);
+                    self.location = "./list";
+                }
+
+                if (res === 0) {
+                    alert("Error !" + res)
+                }
+            }
+        })
+    }
+</script>
 </body>
+
+<script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
+
+<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </html>
